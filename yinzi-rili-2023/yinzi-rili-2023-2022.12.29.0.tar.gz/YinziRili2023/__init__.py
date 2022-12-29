d1f0a3b9cf642ac1d5fc295139b242d8b9c6902a
Tom Forbes <tom@tomforb.es>
2022-12-29 08:44:01 +0000

    {"name":"yinzi-rili-2023","version":"2022.12.29.0","file":"yinzi-rili-2023-2022.12.29.0.tar.gz","path":"yinzi-rili-2023/yinzi-rili-2023-2022.12.29.0.tar.gz"}

---
+++ yinzi-rili-2023/yinzi-rili-2023-2022.12.29.0.tar.gz/YinziRili2023/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yinzi-rili-2023
+https://github.com/apachecn/yinzi-rili-2023"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.29.0"
